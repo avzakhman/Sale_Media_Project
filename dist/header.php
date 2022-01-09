924feed12efbd79ceba28e9a19ea71b1dea36549
Alexander <zaharov4625@gmail.com>
2022-01-09 17:00:59 +0300

    main.css

--- dist/header.php
+++ dist/header.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
-    <link rel="stylesheet" href="css/style.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div class="mobile-menu">
